74e5a86aa959dc72ef06911bc22d49f1cd7e4c47
Meghana-sk <meghana.meghanask.sk@gmail.com>
2023-11-28 22:05:43 +0530

    api: Connect BE to FE to add income

--- frontend/src/components/Incomes/Income.js
+++ frontend/src/components/Incomes/Income.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import { InnerLayout } from '../../styles/Layouts';
-import { useGlobalContext } from '../../context/globalContext';
+import Form from '../Form/Form';
 
 function Income() {
-  const { addIncome } = useGlobalContext();
   return (
     <IncomeStyled>
       <InnerLayout>
         <h1>Incomes</h1>
         <div className='income-content'>
-          <div className='form-container'></div>
+          <div className='form-container'>
+            <Form />
+          </div>
           <div className='incomes'></div>
         </div>
       </InnerLayout>
--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import axios from 'axios';
 
-const BASE_URL = 'hhtp://localhost:5000/api/v1/';
+const BASE_URL = 'http://localhost:5000/api/v1/';
 
 const GlobalContext = React.createContext();
 
@@ -11,13 +11,25 @@ export const GlobalProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const addIncome = async (income) => {
     try {
-      const response = await axios.post(`${BASE_URL}add-income`, income);
+      await axios.post(`${BASE_URL}add-income`, income);
+      getIncomes();
     } catch (error) {
       setError(error.response.data.message);
     }
   };
+
+  const getIncomes = async () => {
+    const response = await axios.get(`${BASE_URL}get-incomes`);
+    setIncomes(response.data);
+    console.log(response.data);
+  };
+
+  const deleteIncome = async (id) => {
+    const res = await axios.delete(`${BASE_URL}delete-income/${id}`);
+    getIncomes();
+  };
   return (
-    <GlobalContext.Provider value={{ addIncome }}>
+    <GlobalContext.Provider value={{ addIncome, error, setError }}>
       {children}
     </GlobalContext.Provider>
   );
