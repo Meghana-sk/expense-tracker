72b432d6572a5501d1f0adadce588dbce4373df7
Meghana-sk <meghana.meghanask.sk@gmail.com>
2023-12-05 18:13:03 +0530

    refactor: History and dashboard min max values

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -7,8 +7,15 @@ import { useGlobalContext } from '../../context/globalContext';
 import History from '../history/History';
 
 function Dashboard() {
-  const { totalIncome, totalExpense, totalBalance, getIncomes, getExpenses } =
-    useGlobalContext();
+  const {
+    totalIncome,
+    totalExpense,
+    totalBalance,
+    getIncomes,
+    getExpenses,
+    incomes,
+    expenses,
+  } = useGlobalContext();
 
   useEffect(() => {
     getIncomes();
@@ -44,6 +51,22 @@ function Dashboard() {
           </div>
           <div className='history-con'>
             <History />
+            <h2 className='salary-title'>
+              Min <span>Salary</span>
+              <span>Max</span>
+            </h2>
+            <div className='salary-item'>
+              <p>{Math.min(...incomes.map((item) => item.amount))}</p>
+              <p>{Math.max(...incomes.map((item) => item.amount))}</p>
+            </div>
+            <h2 className='salary-title'>
+              Min <span>Salary</span>
+              <span>Max</span>
+            </h2>
+            <div className='salary-item'>
+              <p>{Math.min(...expenses.map((item) => item.amount))}</p>
+              <p>{Math.max(...expenses.map((item) => item.amount))}</p>
+            </div>
           </div>
         </div>
       </InnerLayout>
@@ -51,6 +74,81 @@ function Dashboard() {
   );
 }
 
-const DashboardStyled = styled.div``;
+const DashboardStyled = styled.div`
+  .stats-con {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    gap: 2rem;
+    .chart-con {
+      grid-column: 1 / 4;
+      height: 400px;
+      .amount-con {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        gap: 2rem;
+        margin-top: 2rem;
+        .income,
+        .expense {
+          grid-column: span 2;
+        }
+        margin-top: 2rem;
+        .income,
+        .expense,
+        .balance {
+          background-color: #fcf6f9;
+          border: 2px solid #ffffff;
+          box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+          border-radius: 20px;
+          padding: 1rem;
+          p {
+            font-size: 3.5rem;
+            font-weight: 700;
+          }
+        }
+        .balance {
+          grid-column: 2 / 4;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          p {
+            color: var(--color-green);
+            opacity: 0.6;
+            font-size: 4.5rem;
+          }
+        }
+      }
+    }
+    .history-con {
+      grid-column: 4 / -1;
+      h2 {
+        margin: 1rem 0;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
+      .salary-title {
+        font-size: 1.2rem;
+        span {
+          font-size: 1.8rem;
+        }
+      }
+      .salary-item {
+        background-color: #fcf6f9;
+        border: 2px solid #ffffff;
+        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+        border-radius: 20px;
+        padding: 1rem;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        p {
+          font-size: 1.6rem;
+          font-weight: 600;
+        }
+      }
+    }
+  }
+`;
 
 export default Dashboard;
